f3ebd7351f38e6e262f940919cdaf2564ec91ae4
Aniket Agrawal <aniket.mpa@gmail.com>
2023-10-05 22:09:15 +0200

    Add solution for ex 12

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+function Button ({text, handler}) {
+  return (
+    <>
+      <button onClick={handler}>{text}</button>
+    </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [index, setIndex] = useState(0);
+
+  const anecdotes = {
+    0 : 'If it hurts, do it more often.',
+    1 : 'Adding manpower to a late software project makes it later!',
+    2 : 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    3 : 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    4 : 'Premature optimization is the root of all evil.',
+    5 : 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    6 : 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    7 : 'The only way to go fast, is to go well.'
+  };
+
+  function changeIndex() {
+    let newIndex = Math.floor(Object.keys(anecdotes).length * Math.random());
+    setIndex(newIndex);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{anecdotes[index]}</p>
+      <Button text = "next anecdote" handler = {changeIndex} />
     </>
   )
+
 }
 
 export default App
